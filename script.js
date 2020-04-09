b35519aed80dd723ee344879a7d2899b2088a5b4
Obasi Uche <obasiuche62@gmail.com>
2020-04-09 13:35:55 +0100

    created interest calculator

--- script.js
+++ script.js
@@ -26,3 +26,28 @@ let interestCalculator = (Array) => {
 };
 
 interestCalculator(data);
+
+// var interestData = [];
+
+// function interestCalculator(Array) {
+//   for (const obj of Array) {
+//     if (obj.Principal >= 2500 && obj.time > 1 && obj.time < 3) {
+//       obj.rate = 3;
+//     } else if (obj.Principal >= 2500 && obj.time >= 3) {
+//       obj.rate = 4;
+//     } else if (obj.Principal < 2500 && obj.time <= 1) {
+//       obj.rate = 2;
+//     } else {
+//       obj.rate = 1;
+//     }
+
+//     obj.interest = (obj.Principal * obj.rate * obj.time) / 100;
+//     return obj;
+//   }
+
+//   interestData.push(obj);
+//   console.log(interestData);
+//   return interestData;
+// }
+
+// console.log(interestCalculator(data));
